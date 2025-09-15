99cfa807e5104ba262ee7c3f86d882662ac9f617
vishaltrydigitalsolution <vishal@trydigitalsolution.com>
2025-09-15 16:11:10 +0530

    Update home_page.dart

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -10,32 +10,51 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[200],
-      appBar: AppBar(title: Text("API(GET)"),
-      backgroundColor: Colors.grey[200],),
-      body: _ListView(),
-    );
-  }
-  Widget _ListView() {
-    return Obx(
-      () => controller.isLoading.value
-          ? Center(child: CircularProgressIndicator())
-          : controller.product.isEmpty
-          ? Center(child: Text("no data is here"))
-          : ListView.builder(
-              itemCount: controller.product.length,
-              itemBuilder: (context, index) {
-                final data = controller.product[index];
-                return Card(
-                  elevation: 10,
-                  child: ListTile(
-                    leading: Image.network(data.images!.first),
-                    title: Text(data.id.toString()),
-                    subtitle: Text(data.title??""),
+      appBar: AppBar(
+        title: Text("API(GET)"),
+        backgroundColor: Colors.grey[200],
+        actions: [
+          Container(
+              width: 00,
+              height: 50,
+              margin: EdgeInsets.all(8),
+              decoration: BoxDecoration(
+                  color: Colors.grey[300],
+                  borderRadius: BorderRadius.circular(10)
+              ),
+              child:const TextField(
+                decoration: InputDecoration(
+                    border: InputBorder.none,
+                    prefixIcon: Icon(Icons.search,color: Colors.black,)
 
-                  ),
-                );
-              },
-            ),
+                ),
+              )
+          ),
+        ],
+      ),
+      body: Obx(()=>ListView(children: [
+        controller.isLoading.value
+            ? Center(child: CircularProgressIndicator())
+            : controller.product.isEmpty
+            ? Center(child: Text("no data is here"))
+            : ListView.builder(
+          shrinkWrap: true,
+          physics: NeverScrollableScrollPhysics(),
+          itemCount: controller.product.length,
+          itemBuilder: (context, index) {
+            final data = controller.product[index];
+            return Card(
+              elevation: 10,
+              child: ListTile(
+                leading: Image.network(data.images!.first),
+                title: Text(data.id.toString()),
+                subtitle: Text(data.title ?? ""),
+                trailing: Text("\$${data.price}"),
+              ),
+            );
+          },
+        ),
+      ],))
     );
   }
 }
